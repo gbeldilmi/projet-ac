31e3fa3f4dd2e0cc6c9d1c517b1696ce6201d22b
Guillaume BELDILMI <gbeldilmi@pm.me>
2024-03-29 16:27:38 +0100

    Update proto.ml

--- src/proto.ml
+++ src/proto.ml
@@ -4,39 +4,41 @@ type tile = {
   right: int;
   bottom: int;
   left: int;
-}
-
-type board = tile array array
-
-let create_tile id top right bottom left =  { id; top; right; bottom; left }
+} ;;
 
 let create_board size =
-  Array.make_matrix size size (create_tile 0 0 0 0 0)
+  Array.init size (fun _ ->
+    Array.init size (fun _ ->
+      { id=0; top=0; right=0; left=0; bottom=0 }
+    )
+  ) ;;
 
-let print_board board =
+let print_board board = fun () ->
   Array.iter (fun row ->
     Array.iter (fun tile ->
-      Printf.printf "+----------";
+      Printf.printf "+--------------";
     ) row;
     Printf.printf "+\n";
     Array.iter (fun tile ->
-      Printf.printf "|    %d    " tile.top;
+      Printf.printf "|      %02d      " tile.top;
     ) row;
     Printf.printf "|\n";
     Array.iter (fun tile ->
-      Printf.printf "|%d   %d   %d" tile.left tile.id tile.right;
+      Printf.printf "|%02d   %04d   %02d" tile.left tile.id tile.right;
     ) row;
     Printf.printf "|\n";
     Array.iter (fun tile ->
-      Printf.printf "|    %d    " tile.bottom;
+      Printf.printf "|      %02d      " tile.bottom;
     ) row;
     Printf.printf "|\n";
   ) board;
   Printf.printf "+";
   Array.iter (fun _ ->
-    Printf.printf "----------+";
+    Printf.printf "--------------+";
   ) board;
+  Printf.printf "\n"; ;;
 
-let board = create_board 3 in
-print_board board;
+let b = create_board 5;;
+print_board b ();;
+
 
