503a482ae824bd9372ffd8bc98f76ad5109ecd52
ElioGence <83227001+ElioGence@users.noreply.github.com>
2024-04-30 21:47:53 +0200

    code pour LateX

---
+++ doc/sources/code.ml
@@ -0,0 +1,163 @@
+Random.self_init ();;
+
+let size = 12 ;;
+let nbColor = 10 ;;
+
+type tile = {
+  id: int;
+  top: int;
+  right: int;
+  bottom: int;
+  left: int;
+} ;;
+
+let create_board size =
+  Array.init size (fun _ ->
+      Array.init size (fun _ ->
+          { id=(-1); top=0; right=0; left=0; bottom=0}
+        )
+    ) ;; 
+
+let init_board size =
+  let board = create_board size in
+  for i = 0 to size - 1 do
+    if i mod 2 = 0 then
+      for j = 0 to size-1 do
+        board.(i).(j) <-
+          {
+            top = if i = 0 then 0 else board.(i-1).(j).bottom;
+            bottom = if i = size-1 then 0 else Random.int nbColor + 1;
+            left = if j = 0 then 0 else board.(i).(j - 1).right;
+            right = if j = size - 1 then 0 else Random.int nbColor + 1;
+            id = i*size+j; 
+          };
+      done
+    else 
+      for j = size-1 downto 0 do
+        board.(i).(j) <-
+          {
+            top = board.(i-1).(j).bottom;
+            bottom = if i = size-1 then 0 else Random.int nbColor + 1;
+            left = if j = 0 then 0 else Random.int nbColor + 1;
+            right = if j = size - 1 then 0 else board.(i).(j + 1).left;
+            id = i*size+j; 
+          };
+      done;
+  done; 
+  board;;
+
+let print_board = fun board ->
+  Array.iter (fun row ->
+      Array.iter (fun tile ->
+          Printf.printf "+--------------";
+        ) row;
+      Printf.printf "+\n";
+      Array.iter (fun tile ->
+          Printf.printf "|      %02d      " tile.top;
+        ) row;
+      Printf.printf "|\n";
+      Array.iter (fun tile ->
+          Printf.printf "|%02d   %04d   %02d" tile.left tile.id tile.right;
+        ) row;
+      Printf.printf "|\n";
+      Array.iter (fun tile ->
+          Printf.printf "|      %02d      " tile.bottom;
+        ) row;
+      Printf.printf "|\n";
+    ) board;
+  Printf.printf "+";
+  Array.iter (fun _ ->
+      Printf.printf "--------------+";
+    ) board;
+  Printf.printf "\n" ;;
+
+
+let shuffle_board board =
+  for i = 0 to size - 1 do
+    for j = 0 to size - 1 do
+      let random_i = Random.int size in
+      let random_j = Random.int size in
+      let temp_tile = board.(i).(j) in
+      board.(i).(j) <- board.(random_i).(random_j);
+      board.(random_i).(random_j) <- temp_tile;
+    done;
+  done;
+  board ;;
+
+let init_tiles board=
+  let tiles = Array.make (size*size) ({ id=(-1); top=0; right=0; left=0; bottom=0}) in
+  for i = 0 to size - 1 do
+    for j = 0 to size - 1 do
+      tiles.((i)*size+j) <- board.(i).(j);
+    done;
+  done;
+  tiles ;; 
+
+let rec solve_backtrack board tiles i j =
+  
+  let test_tile t i j =
+    let res = ref true in
+    if j != 0 && board.(i).(j - 1).right != tiles.(t).left then
+      res := false
+    else if j != 0 && tiles.(t).left = 0 then
+      res := false
+    else if j != (size - 1) && tiles.(t).right = 0 then
+      res := false
+    else if i != 0 && board.(i - 1).(j).bottom != tiles.(t).top then
+      res := false
+    else if i != 0 && tiles.(t).top = 0 then
+      res := false
+    else if i != (size - 1) && tiles.(t).bottom = 0 then
+      res := false
+    else if i = 0 && (tiles.(t).top != 0) then
+      res := false
+    else if i = (size - 1) && (tiles.(t).bottom != 0) then
+      res := false
+    else if j = 0 && (tiles.(t).left != 0) then
+      res := false
+    else if j = (size - 1) && (tiles.(t).right != 0) then
+      res := false;
+    !res; 
+  in 
+  
+  if board.(i).(j).id = -1 then
+    for t = 0 to (Array.length tiles) -1 do
+      if tiles.(t).id != -1 then
+        if test_tile t i j then begin
+          board.(i).(j) <- tiles.(t);
+          tiles.(t) <- { id = -1; top = 0; right = 0; bottom = 0; left = 0};
+          if i < size - 1 then 
+            solve_backtrack board tiles (i+1) j
+          else if j < size - 1 then
+            solve_backtrack board tiles 0 (j+1)
+          else 
+            print_board board;
+          tiles.(t) <- board.(i).(j);
+          board.(i).(j) <- { id = -1; top = 0; right = 0; bottom = 0; left = 0}; 
+        end
+    done 
+  else if i < size - 1 then 
+    solve_backtrack board tiles (i+1) j
+  else if j < size - 1 then
+    solve_backtrack board tiles 0 (j+1)
+  else 
+    print_board board;
+;;
+
+
+(* main *)
+
+let b = init_board size ;;
+Printf.printf "Initial board\n" ;;
+print_board b ;;
+
+Printf.printf "\n\nShuffled board\n" ;;
+let b = shuffle_board b ;;
+print_board b ;;
+
+Printf.printf "\n\nSolving: may the time be with you\n";;
+let tiles = init_tiles b;;
+let board = create_board size;;
+solve_backtrack board tiles 0 0; 
+Printf.printf "\n\nSolving: end of time\n" ;
+
---
+++ doc/sources/proto.py
@@ -0,0 +1,104 @@
+import random, sys
+
+sys.setrecursionlimit(1000000)
+
+class Tile:
+  def __init__(self, id, top, right, bottom, left):
+    self.id = id
+    self.top = top
+    self.right = right
+    self.bottom = bottom
+    self.left = left
+
+class Eternity2:
+  def __init__(self, colors, size):
+    self.colors = colors
+    self.size = size
+    self.board = [[None for _ in range(size)] for _ in range(size)]
+    self.tiles = []
+  def __str__(self):
+    s = "+"
+    for j in range(self.size):
+      s += "----------+" * self.size + "\n|"
+      for i in range(self.size):
+        s += "    {:02d}    |".format(self.board[i][j].top) if self.board[i][j] is not None else "    <>    |"
+      s += "\n|"
+      for i in range(self.size):
+        s += "{:02d} {:04d} {:02d}|".format(self.board[i][j].left, self.board[i][j].id, self.board[i][j].right) if self.board[i][j] is not None else "<> <<>> <>|"
+      s += "\n|"
+      for i in range(self.size):
+        s += "    {:02d}    |".format(self.board[i][j].bottom) if self.board[i][j] is not None else "    <>    |"
+      s += "\n+"
+    s += "----------+" * self.size + "\n"
+    return s
+  def save_to_file(self, filename):
+    with open(filename, "w") as f:
+      f.write(str(self))
+  def init_grid(self):
+    nt = 0
+    for j in range(self.size):
+      for i in range(self.size):
+        self.board[i][j] = Tile(nt,
+          random.randint(0, self.colors - 1) if j == 0             else self.board[i][j - 1].bottom,
+          random.randint(0, self.colors - 1) if i != self.size - 1 else self.board[0][j].left,
+          random.randint(0, self.colors - 1) if j != self.size - 1 else self.board[i][0].top,
+          random.randint(0, self.colors - 1) if i == 0             else self.board[i - 1][j].right)
+        nt += 1
+    print("init_grid: grid created\n" + str(self))
+  def shuffle(self):
+    assert self.board[0][0] is not None
+    assert self.tiles == []
+    for i in range(self.size):
+      for j in range(self.size):
+        self.tiles.append(self.board[i][j])
+        self.board[i][j] = None
+    assert len(self.tiles) == self.size * self.size
+    random.shuffle(self.tiles)
+    print("shuffle: grid shuffled\n" + str(self))
+  def solve(self):
+    def solve_backtrack(i, j):
+      def test_tile(t, i, j):
+        if j != 0:
+          if self.tiles[t].top != self.board[i][j - 1].bottom:
+            return False
+        if i == self.size - 1:
+          if self.tiles[t].right != self.board[0][j].left:
+            return False
+        if j == self.size - 1:
+          if self.tiles[t].bottom != self.board[i][0].top:
+            return False
+        if i != 0:
+          if self.tiles[t].left != self.board[i - 1][j].right:
+            return False
+        return True
+      if self.board[i][j] is None:
+        for t in range(len(self.tiles)):
+          if self.tiles[t] is not None:
+            if test_tile(t, i, j):
+              self.board[i][j] = self.tiles[t]
+              self.tiles[t] = None
+              if i < self.size - 1:
+                solve_backtrack(i + 1, j)
+              elif j < self.size - 1:
+                solve_backtrack(0, j + 1)
+              else:
+                print("solve_backtrack: found solution\n" + str(self))
+              self.tiles[t] = self.board[i][j]
+              self.board[i][j] = None
+      else:
+        if i < self.size - 1:
+          solve_backtrack(i + 1, j)
+        elif j < self.size - 1:
+          solve_backtrack(0, j + 1)
+        else:
+          print("solve_backtrack: found solution\n" + str(self))
+      return
+    print("solve: may the time be with you")
+    solve_backtrack(0, 0)
+    print("solve: end of time")
+
+if __name__ == "__main__":
+  e2 = Eternity2(10, 12)
+  e2.init_grid()
+  e2.shuffle()
+  e2.solve()
