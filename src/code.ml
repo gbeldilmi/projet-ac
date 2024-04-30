889ee247fc40154a7c89bb26ae3fb2d6c2411be7
Guillaume BELDILMI <gbeldilmi@pm.me>
2024-04-30 08:14:37 +0200

    Update code.ml

--- src/code.ml
+++ src/code.ml
@@ -91,6 +91,7 @@ let init_board size =
   board;;
 
 let print_board = fun board ->
+  Printf.printf "<deb>\n";
   Array.iter (fun row ->
       Array.iter (fun tile ->
           Printf.printf "+--------------";
@@ -113,7 +114,7 @@ let print_board = fun board ->
   Array.iter (fun _ ->
       Printf.printf "--------------+";
     ) board;
-  Printf.printf "\n" ;;
+  Printf.printf "\n\n" ;;
 
 
 let shuffle_board board =
